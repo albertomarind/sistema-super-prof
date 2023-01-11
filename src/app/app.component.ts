abf24d1d9327a7b89c5155646ba28158af1230a6
Alberto Marin <alberto.marind@gmail.com>
2023-01-11 12:55:33 -0600

    Primera version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sistema';
+
+  productos:any[] = [
+    {
+      id:1,
+      titulo:"titulo1",
+      descripcion:"descripcion1",
+      precio:15.51
+    },
+    {
+      id:2,
+      titulo:"titulo2",
+      descripcion:"descripcion2",
+      precio:15.52
+    },
+    {
+      id:3,
+      titulo:"titulo3",
+      descripcion:"descripcion3",
+      precio:15.53
+    }
+  ];
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  id:number = 0;
+
+  @Input()
+  titulo:string = '';
+
+  @Input()
+  descripcion:string = '';
+
+  @Input()
+  precio:number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
